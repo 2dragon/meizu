ae7e1f03bcb664dbf83f8a7fa0726326a5b35676
jsl <920024814@qq.com>
2020-08-13 22:09:02 +0800

    完成了顶部下拉框，广告部分，第一个商品展示列表部分

--- script/index.js
+++ script/index.js
@@ -1,9 +1,40 @@
 var mySwiper = new Swiper(".swiper-container", {
-  direction: "horizontal", // 垂直切换选项
+  direction: "horizontal", // 切换方向选项
   loop: true, // 循环模式选项
 
   // 如果需要分页器
   pagination: {
     el: ".swiper-pagination",
   },
+});
+
+$(".hoverdown-content").addClass("notshow");
+$(".top-nav-wrap").css("background", "");
+$(".top-nav-right-ul").on("mouseenter", "li", function () {
+  $(".hoverdown-content").addClass("notshow");
+  var index = $(this).index();
+  $(".top-nav-wrap").addClass("whitebg");
+  console.log(index,"我不是");
+  if ("01239".includes(index)) {
+    console.log($(this).index() + "进来了");
+    $(".hoverdown-content").eq(index).removeClass('notshow');
+    $(".hoverdownwrap").slideDown(30, "swing", function () {});
+  }
+  else{
+    $(".hoverdown-content").addClass("notshow");
+    $(".hoverdownwrap").slideUp(30, "swing", function () {
+      $(".top-nav-wrap").removeClass("whitebg");
+    });
+  }
+});
+
+$(".top-nav-right-uldown").on("mouseleave", function () {
+  var index = $(this).index();
+  console.log(index + "转身离开");
+  $(".hoverdown-content").addClass("notshow");
+  $(".hoverdownwrap").slideUp(30, "swing", function () {
+    $(".top-nav-wrap").removeClass("whitebg");
+  });
+  console.log('我调用222');
+
 });
