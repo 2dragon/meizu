c695442fadc14caf6023dd624ded015006611eb0
jsl <920024814@qq.com>
2020-08-18 18:00:07 +0800

    静态渲染改为动态渲染，完善数据，完善细节

--- script/detail.js
+++ script/detail.js
@@ -1,25 +1,126 @@
-//初始化：关闭框内显示，清除背景颜色
+//头部
+// 初始化：关闭框内显示，清除背景颜色
 $(".hoverdown-content").addClass("notshow");
 $(".top-nav-wrap").css("background", "");
+
 $(".top-nav-right-ul").on("mouseenter", "li", function () {
+  $(".navitem a").addClass("blackpink");
   //关闭所有的框内显示
   $(".hoverdown-content").addClass("notshow");
   var index = $(this).index();
   $(".top-nav-wrap").addClass("whitebg");
+  console.log(index, "我不是");
   if ("01239".includes(index)) {
+    // $(".top-nav-left svg").attr("fill", "#008cff");
+
+    $(".navitem a").addClass("blackpink");
+
+    console.log($(".navitem a").hasClass("blackpink"));
+    console.log($(this).index() + "进来了");
     //打开指定的内容显示
     $(".hoverdown-content").eq(index).removeClass("notshow");
     $(".hoverdownwrap").slideDown(30, "swing", function () {});
   } else {
+    // $(".top-nav-left svg").attr("fill", "#fff");
     $(".hoverdown-content").addClass("notshow");
     $(".hoverdownwrap").slideUp(30, "swing", function () {
       $(".top-nav-wrap").removeClass("whitebg");
+      $(".navitem a").removeClass("blackpink");
     });
   }
 });
 $(".top-nav-right-uldown").on("mouseleave", function () {
+  $(".navitem a").removeClass("blackpink");
+  // $(".top-nav-left svg").attr("fill", "#fff");
+  var index = $(this).index();
+  console.log(index + "转身离开");
   $(".hoverdown-content").addClass("notshow");
   $(".hoverdownwrap").slideUp(30, "swing", function () {
     $(".top-nav-wrap").removeClass("whitebg");
   });
+  console.log("我调用222");
+});
+//渲染数据
+$.ajax({
+  url: "../json/detail.json",
+  type: "get",
+  dataType: "json",
+  success: function (jsonArr) {
+    $.each(jsonArr, function (index, item) {
+      console.log("可怕你怎么进不来");
+      console.log(item.goodsnum);
+      if (item.goodsnum == window.location.search.substr(10)) {
+        console.log("true");
+        var bigimg = ` <img src="${item.imgs[0]}" alt="">`;
+        $(".detail-left-bigimg").append(bigimg);
+
+        $.each(item.imgs, function (index, item) {
+          console.log(item);
+          var smallimg = ` <img src="${item}" alt="">`;
+          $(".detail-left-smallimg").append(smallimg);
+        });
+
+        $(".detail-right h1").append(item.tit);
+        $(".detail-right p").append(item.desc);
+        $(".price1").append(item.newprice);
+        $(".delprice").append(item.oldprice);
+      }
+    });
+  },
+});
+
+
+
+
+
+
+//点击选框变化颜色
+$(".detail-left-smallimg").on("click", "img", function () {
+  $(".detail-left-bigimg img").prop("src", $(this).prop("src"));
+});
+
+$(".xiangguan .part2-cont .part2-cont-box").eq(0).css("color", "#000");
+$(".xiangguan .part2-cont .part2-cont-box")
+  .eq(0)
+  .css("border", "1px solid #000");
+$(".xiangguan .part2-cont").on("click", ".part2-cont-box", function () {
+  $(".xiangguan .part2-cont .part2-cont-box").css("color", "#bfbfbf");
+  $(".xiangguan .part2-cont .part2-cont-box").css(
+    "border",
+    "1px solid #bfbfbf"
+  );
+  $(this).css("color", "#000");
+  $(this).css("border", "1px solid #000");
+});
+
+$(".yanse .part2-cont .part2-cont-box").eq(0).css("color", "#000");
+$(".yanse .part2-cont .part2-cont-box").eq(0).css("border", "1px solid #000");
+$(".yanse .part2-cont").on("click", ".part2-cont-box", function () {
+  $(".yanse .part2-cont .part2-cont-box").css("color", "#bfbfbf");
+  $(".yanse .part2-cont .part2-cont-box").css("border", "1px solid #bfbfbf");
+  $(this).css("color", "#000");
+  $(this).css("border", "1px solid #000");
+});
+console.log($(".huabei .part2-cont .part2-cont-box").eq(0).children());
+$(".huabei .part2-cont .part2-cont-box").eq(0).children().css("color", "#000");
+$(".huabei .part2-cont .part2-cont-box").eq(0).css("border", "1px solid #000");
+$(".huabei .part2-cont").on("click", ".part2-cont-box", function () {
+  $(".huabei .part2-cont .part2-cont-box").children().css("color", "#bfbfbf");
+  $(".huabei .part2-cont .part2-cont-box").css("border", "1px solid #bfbfbf");
+  $(this).children().css("color", "#000");
+  $(this).css("border", "1px solid #000");
+});
+// 加减
+$(".jian").on("click", function () {
+  if ($(".shu").text() > 1) {
+    $(".shu").text($(".shu").text() - 1);
+  } else {
+    $(".jian").css("color", "#e0e0e0");
+  }
+});
+$(".jia").on("click", function () {
+  $(".shu").text(1 + parseInt($(".shu").text()));
+  if ($(".shu").text() >= 1) {
+    $(".jian").css("color", "#000");
+  }
 });
--- script/index.js
+++ script/index.js
@@ -1,27 +1,64 @@
-var mySwiper = new Swiper(".swiper-container", {
-  direction: "horizontal", // 切换方向选项
-  loop: true, // 循环模式选项
+$.ajax({
+  url: "../json/top.json",
+  type: "get",
+  dataType: "json",
+  success: function (jsonArr) {
+    //渲染数据
 
-  // 如果需要分页器
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  on: {
-    slideChangeTransitionEnd: function (swiper, event) {
-      console.log("触发改变了", this.activeIndex);
-      //你的事件
-      if ("0124578".includes(this.activeIndex)) {
-        $(".navitem a").css("color", "#fff");
-        $(".top-nav-left svg").attr("fill", "#fff");
-        $(".top-nav-right-people img").attr("src", "../image/people3.png");
-        $(".top-nav-right-buycar img").attr("src", "../image/buycar3.png");
-      } else {
-        $(".navitem a").css("color", "#333");
-        $(".top-nav-left svg").attr("fill", "#008cff");
-        $(".top-nav-right-people img").attr("src", "../image/people.png");
-        $(".top-nav-right-buycar img").attr("src", "../image/buycar2.png");
+    $.each(jsonArr, function (index, item) {
+      console.log(item.goodsnum.slice(0, 1));
+      console.log(item.goodsnum);
+      switch (item.goodsnum.slice(0, 1)) {
+        case "1":
+          console.log("执行",1);
+          var goodsDom1 = ` <div class="swiper-slide">
+      <a href="./detail.html?goodsnum=${item.goodsnum}">
+        <img src="${item.src}" alt="1">
+      </a>
+    </div>`;
+          $(".swiper-wrapper").append(goodsDom1);
+          break;
+
+        case "4":
+        var goodsDom4 = ` 
+        <div class="fourshow-goods yinying">
+          <img src="${item.src}" alt="">
+          <div class="goodstit">
+            <h1>${item.name}</h1>
+            <p>${item.desc}</p>
+            <h2> <i>￥</i>${item.price}<em></em><s></s></h2>
+          </div>
+        </div>`;
+          $(".fourshow").append(goodsDom4);
+          break;
       }
-    },
+    });
+
+    var mySwiper = new Swiper(".swiper-container", {
+      direction: "horizontal", // 切换方向选项
+      loop: true, // 循环模式选项
+      // 如果需要分页器
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      on: {
+        slideChangeTransitionEnd: function (swiper, event) {
+          console.log("触发改变了", this.activeIndex);
+          //你的事件
+          if ("012578".includes(this.activeIndex)) {
+            $(".navitem a").css("color", "#fff");
+            $(".top-nav-left svg").attr("fill", "#fff");
+            $(".top-nav-right-people img").attr("src", "../image/people3.png");
+            $(".top-nav-right-buycar img").attr("src", "../image/buycar3.png");
+          } else {
+            $(".navitem a").css("color", "#333");
+            $(".top-nav-left svg").attr("fill", "#008cff");
+            $(".top-nav-right-people img").attr("src", "../image/people.png");
+            $(".top-nav-right-buycar img").attr("src", "../image/buycar2.png");
+          }
+        },
+      },
+    });
   },
 });
 
@@ -30,7 +67,7 @@ $(".hoverdown-content").addClass("notshow");
 $(".top-nav-wrap").css("background", "");
 
 $(".top-nav-right-ul").on("mouseenter", "li", function () {
-  $(".navitem a").addClass('blackpink');
+  $(".navitem a").addClass("blackpink");
   //关闭所有的框内显示
   $(".hoverdown-content").addClass("notshow");
   var index = $(this).index();
@@ -41,7 +78,7 @@ $(".top-nav-right-ul").on("mouseenter", "li", function () {
 
     $(".navitem a").addClass("blackpink");
 
-    console.log($(".navitem a").hasClass('blackpink'));
+    console.log($(".navitem a").hasClass("blackpink"));
     console.log($(this).index() + "进来了");
     //打开指定的内容显示
     $(".hoverdown-content").eq(index).removeClass("notshow");
@@ -57,7 +94,7 @@ $(".top-nav-right-ul").on("mouseenter", "li", function () {
 });
 
 $(".top-nav-right-uldown").on("mouseleave", function () {
-  $(".navitem a").removeClass('blackpink');
+  $(".navitem a").removeClass("blackpink");
   $(".top-nav-left svg").attr("fill", "#fff");
   var index = $(this).index();
   console.log(index + "转身离开");
